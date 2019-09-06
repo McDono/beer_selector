bd5102369d8879e2bfe2a12684f838ea6dcda386
Donovan <mcdonovan@outlook.fr>
2019-09-06 16:32:25 -0500

    beer selector V1.0; works on simulator

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	private answers = [
+		'GREAT RAFT DDH COMMOTION',
+		'BELL\'S SMITTEN GOLDEN RYE',
+		'CRYING EAGLE THINGS UNSETTLED',
+    'FLYING TIGER JUCEE',
+		'LAGUNITAS SUPER CLUSTER',
+		'NATCHEZ BEBELO',
+   	'PARISH PURE TROPICS',
+		'SAMUEL ADAMS OCTOBERFEST',
+		'TERRAPIN LUAU KRUNKLES',
+   	'ABITA AMBER',
+		'ABITA ANDYGATOR',
+		'ABITA HONEYSUCKLE SAISON',
+   	'ABITA PURPLE HAZE',
+		'ABITA STRAWBERRY HARVEST',
+   	'Concentrate and ask again.',
+		'ABITA THE BOOT',
+		'ANCHOR STEAM',
+   	'ANCHOR STEAM',
+		'ARROGANT BASTARD ALE',
+		'BELL\'S TWO HEARTED ALE']
+	constructor(private toastCtrl: ToastController) {}
 
-  constructor() {}
+	giveAnswer() {
+		let answer = this.answers[Math.floor(Math.random()*this.answers.length)];
+		this.presentToast(answer);
+	}
+
+	async presentToast(answer) {
+		const toast = await this.toastCtrl.create({
+			message: answer,
+			duration: 5000,
+			position: 'top'
+		});
+		toast.present();
+	}
+
+
 
 }
